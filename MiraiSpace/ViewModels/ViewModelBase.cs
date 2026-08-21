7269c8ed8c8a6deffc79a4878b0c91560b7d5382
Lewis <lakti02@mail.ru>
2026-08-21 19:05:50 +0500

    init

---
+++ MiraiSpace/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MiraiSpace.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
---
+++ MiraiSpace/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace MiraiSpace.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
